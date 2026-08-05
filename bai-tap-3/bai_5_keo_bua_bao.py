09120be5c53c6abdf3d6c21cd95696b1082e220d
HoaCG <hoanguyen333@gmail.com>
2026-08-05 21:05:08 +0700

    cau 5 (da hoan thanh) va cau 6 (file rong)

---
+++ bai-tap-3/bai_5_keo_bua_bao.py
@@ -0,0 +1,101 @@
+import random
+
+"""
+CÁC ĐOẠN CODE SAU ĐỀU DO NGƯỜI VIẾT, KHÔNG SỬ DỤNG AI
+"""
+
+moves = ['keo', 'bao', 'bua']
+
+def get_random_move():
+    return random.choice(moves)
+
+def play(input1, input2):
+    if input1 == input2:
+        return 0
+
+    if (input1 == 'keo' and input2 == 'bao') or (input1 == 'bua' and input2 == 'keo') or (input1 == 'bao' and input2 == 'bua'):
+        return 1
+    else:
+        return 2
+
+"""
+Hàm nhận vào 1 cặp đấu và tìm ra người thắng cuộc
+Nếu hòa thì sẽ chơi lại
+"""
+def match(pairs):
+    player1, player2 = pairs
+
+    if player2 == -1:
+        return player1
+
+    print(f'===Match: Player {player1} vs Player {player2}')
+    player1_move = get_random_move()
+    player2_move = get_random_move()
+    result = play(player1_move, player2_move)
+
+    while result == 0:
+        print("It's a tie! play again")
+        player1_move = get_random_move()
+        player2_move = get_random_move()
+        result = play(player1_move, player2_move)
+
+    if result == 1:
+        print(f'Player {player1} wins!')
+        return player1
+
+    print(f'Player {player2} wins!')
+    print("===End Match\n", flush=True)
+    return player2
+
+"""
+Hàm chia cặp đấu, mảng các người chơi và tự chia cặp, nếu là số lẻ thì người chơi cuối sẽ cặp với -1
+
+get_pairs([1, 2, 3]) -> [(1, 2), (3, -1)]
+get_pairs([1, 2, 3, 4]) -> [(1, 2), (3, 4)]
+
+"""
+
+def get_pairs(players):
+    pairs = [(players[i], players[i + 1]) for i in range(0, len(players) - 1, 2)]
+
+    if len(pairs) * 2 < len(players):
+        pairs.append((players[-1], -1))
+
+    return pairs
+
+def print_pairs(pairs):
+    print("===Matches:")
+    for (player1, player2) in pairs:
+        if player2 != -1:
+            print(f'Player {player1} vs Player {player2}')
+
+    print("===")
+
+"""
+Hàm chạy giải đấu, nhận vào số lượng người chơi, tạo ra danh sach người chơi (danh sách sô nguyên)
+Sau đó danh sách được trộn lẫn
+Tiếp theo được chia cặp, dùng hàm get_pair
+Người thắng của mỗi cặp sẽ được vào vòng tiếp theo
+Cho tới khi chỉ còn 1 căp với "người chơi" thứ 2 là -1 -> Nghĩa là người còn lại trong cặp đó là champion
+"""
+def league(number_of_players):
+    players = list(range(number_of_players))
+    random.shuffle(players)
+    pairs = get_pairs(players)
+
+    while not (len(pairs) == 1 and pairs[-1][1] == -1):
+        print("===================START ROUND=======================")
+        print_pairs(pairs)
+        winners = [ match(pair) for pair in pairs ]
+        pairs = get_pairs(winners)
+        print("===================END ROUND=======================")
+
+    print(f"Player {pairs[-1][0]} is the champion!!")
+
+
+def main():
+    league(number_of_players=random.randint(8, 20))
+
+
+if __name__ == '__main__':
+    main()
